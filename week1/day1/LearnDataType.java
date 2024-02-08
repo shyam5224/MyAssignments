1e4ecaddd4069e069c8e50246b2f10e4fd94d536
shyam5224 <158989181+shyam5224@users.noreply.github.com>
2024-02-08 19:24:29 +0530

    Add files via upload
    Week 1 - Day1 assignments

---
+++ week1/day1/LearnDataType.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+
+public class LearnDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion=(float)32.0;
+		String browserName="Firefox";
+		boolean pageLoaded=true;
+		int noOfBrowser=2;
+		char browserLogo='F';
+		System.out.println("Browser Version: "+browserVersion);
+		System.out.println("Browser Name: "+browserName);
+		System.out.println("Page Loaded: "+pageLoaded);
+		System.out.println("No of Browser: "+noOfBrowser);
+		System.out.println("Browser Logo: "+browserLogo);
+
+	}
+
+}
---
+++ week1/day1/LearnIfCondition.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class LearnIfCondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var=45;
+		if(var>0)
+		{
+			System.out.println("The number is positive");
+		}
+		else
+		{
+			System.out.println("The number is negative");
+		}
+		
+
+	}
+
+}
+
